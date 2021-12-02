abf6b58b8be8635b9e85039e271f34f0fdc2cdd2
Uriyah Ann <uriyahruiz1@gmail.com>
2021-12-01 21:29:42 -0800

    Client Setup

---
+++ client/src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <h1> Main Component </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
